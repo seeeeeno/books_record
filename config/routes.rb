47a1cfec10d399d8acd3ddd2694a38160ff674de
seeeeeno <ittdhitfhhj864269@gmail.com>
2020-08-28 12:03:48 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
-  resources :books, only: [:index, :new, :create, :show, :edit, :update]
+  resources :books
   resources :users, only: [:index]
 end

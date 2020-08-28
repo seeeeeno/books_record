7620fcec986fa40759985a7ad588835a6e4845b8
seeeeeno <ittdhitfhhj864269@gmail.com>
2020-08-28 12:09:53 +0900

    booksコントローラーにdestroyアクションを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,6 +35,10 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
